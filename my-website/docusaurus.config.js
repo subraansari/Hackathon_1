442f0ebcaf32f900f651b1c0d9b41bd99ca01e55
Subra Ansari <164074216+subraansari@users.noreply.github.com>
2026-02-05 23:00:20 +0500

    first commit

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://humanoid-robotics-education.github.io', // Replace with your actual URL
+  url: 'https://hackathon-1-omega-seven.vercel.app/', // Replace with your actual URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
